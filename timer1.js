429f0eb0fced384a45fb7e19e0e041be046d11ac
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-19 11:24:47 +0000

    updating the funtion

--- timer1.js
+++ timer1.js
@@ -2,9 +2,7 @@ const simpleTimer = function(inputTimer) {
 
   for (let i = 0; i < inputTimer.length; i++) {
 
-    if (inputTimer.length === 0) {
-      break;
-    } else if (inputTimer[i] > 0) {
+    if (inputTimer[i] > 0) {
       setTimeout(() => {
         process.stdout.write('\x07');
       }, inputTimer[i] * 1000);
